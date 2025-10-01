8199adf7223e3eb051aa02752a8ffb22e75198ed
niyongaboemmy <emmanuelniyongabo44@gmail.com>
2025-10-01 17:57:27 +0200

    Updates on install prompt

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: false, // Enable PWA in development for testing
   register: true,
   skipWaiting: true,
   runtimeCaching: [
